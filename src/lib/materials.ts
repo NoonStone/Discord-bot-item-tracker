7c1ee7400870747ac50c09910b8dd3eaf3b9ae65
WageMe <wagemeofficial@hotmail.com>
2025-05-31 21:07:13 -0400

    WageMe(streamline) Made code denser, +lib, +items

---
+++ src/lib/materials.ts
@@ -0,0 +1,19 @@
+export function getMaterialList(): string[] {
+	return [
+		'diamond',
+		'gold',
+		'iron',
+		'coal',
+		'lapis',
+		'redstone',
+		'emeralds',
+		'copper',
+		'sets',
+		'helmet',
+		'chestplate',
+		'leggings',
+		'boots',
+		'swords',
+		'xp'
+	];
+}
